d8fca3c5f4988e97ef1a02ddc051cdff1f68589d
ONDA, Takashi <onda@mmj.ne.jp>
2015-09-18 19:20:54 +0900

    json writer

--- app/controllers/Index.scala
+++ app/controllers/Index.scala
@@ -1,19 +1,24 @@
 package controllers
 
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import play.api.mvc._
 import models._
 
 class Index extends Controller {
 
-//  implicit val userWriter: Writes[User] = Json.writes[User]
+  implicit val userWriter: Writes[User] = (
+    (JsPath \ "id").write[Int] and
+     (JsPath \ "name").write[String]
+  )(unlift(User.unapply))
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 
   def user(id: Int) = Action {
-    Ok(User.find(id).getOrElse(User(0, "unknown")).name)
+    val u = User.find(id).getOrElse(User(0, "unknown"))
+    Ok(Json.toJson(u))
   }
 
 }
