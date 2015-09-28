00b8b453644e7802a921e8e0047ab74407b674ee
ONDA, Takashi <onda@mmj.ne.jp>
2015-09-28 14:04:13 +0900

    partial function で読みやすく

--- app/models/Dao.scala
+++ app/models/Dao.scala
@@ -15,16 +15,18 @@ case class Dao(b: SelectSQLBuilder[User], conditions: List[Option[SQLSyntax]] = 
   def like(column: SQLSyntax, key: String): Option[SQLSyntax] = Some(sqls.like(column, s"%${key}%"))
 
   def byUserName(key: String) = Dao(b, like(u.name, key) :: conditions)
+
   def byProjectName(key: String) = Dao(b, like(p.name, key) :: conditions)
 
-  def tpl(u: SyntaxProvider[User], p: SyntaxProvider[Project])(rs: WrappedResultSet): (User, Project) = (Users(u.resultName)(rs), Projects(p.resultName)(rs))
+  def tpl(u: SyntaxProvider[User], p: SyntaxProvider[Project])(rs: WrappedResultSet): (User, Project) =
+    (Users(u.resultName)(rs), Projects(p.resultName)(rs))
 
   def apply(): Iterable[SearchResult] = {
     withSQL {
       b.where(sqls.toAndConditionOpt(conditions: _*))
     }.map(tpl(u, p)).list().apply().
-      groupBy { case (u: User, p: (Project)) => u }.
-      map { case (u: User, g: List[(User, Project)]) => SearchResult(u, g.map(_._2))}
+      groupBy { case (u: User, p: Project) => u }.
+      map { case (u: User, g: List[(User, Project)]) => SearchResult(u, g.map(_._2)) }
   }
 
 }
